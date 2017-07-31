725ebe3ab7b1e72085395bdff9d220bcbb200047
Savin Sergey <savin@kartel.ru>
2017-07-31 12:11:37 +0300

    simple recieve request

--- include/transport.hpp
+++ include/transport.hpp
@@ -1,8 +1,27 @@
 #pragma once
 
+#include "settings.hpp"
+
 namespace Transport
 {
 class TransportImpl
 {
+public:
+	explicit TransportImpl(const Setting::Settings& settings);
+	~TransportImpl();
+	TransportImpl(const TransportImpl&& other);
+	TransportImpl& operator=(const TransportImpl&&);
+	
+	void initialise();
+	std::string getWhiteList(const std::string& query) const;
+	
+private:
+	Setting::Settings settings_;
+	CURL *curl_ = nullptr;
+	std::string buffer_;
+
+	// disable copy, assign. enable only move
+	TransportImpl(const TransportImpl&) = delete;
+	TransportImpl& operator=(const TransportImpl&) = delete;
 };
 }
